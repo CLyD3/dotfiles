55989d9505aa56da796c5f85e6a3046054dea667
CLyD <rsvp@mighty.(none)>
2016-02-07 20:24:02 +0100

    added rec search function

--- .bashrc.d/.functions.bash
+++ .bashrc.d/.functions.bash
@@ -2,3 +2,11 @@
 cp_p () {
   rsync -WavP --human-readable --progress $1 $2
 }
+
+
+#Search recursiv
+qt() {
+    grep -ir --color=always "$*" . | less -RX
+    #     │└─ search all files under each directory, recursively
+    #     └─ ignore case
+}
