ac9d1629b55bc0f534aab9b11f91edc44e24839d
rsvp <rsvp@mighty.(none)>
2016-02-07 16:54:12 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXTRA_DIR="$HOME/.extra"
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/.bashrc.d" ~
+ln -sfv "$DOTFILES_DIR/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
